805bba09ee8365c09eedd052de512824a5d9163e
Shashank <s_shashank@outlook.com>
2020-12-01 23:14:43 +0530

    custom fonts and flex properties

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
